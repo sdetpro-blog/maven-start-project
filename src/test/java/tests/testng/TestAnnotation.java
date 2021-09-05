2e6b26b2767b5bc5195b5313623a5a18c2841963
Tu Huynh <tuhuynh@Tus-MacBook-Pro.attlocal.net>
2021-09-05 08:01:00 -0500

    TestNG setup

---
+++ src/test/java/tests/testng/TestAnnotation.java
@@ -0,0 +1,19 @@
+package tests.testng;
+
+import org.testng.annotations.Test;
+
+public class TestAnnotation {
+
+
+    @Test
+    public void a1(){
+        System.out.println(">>>> testAnnotation");
+    }
+
+
+    @Test
+    public void a2(){
+        System.out.println(">>>> test2ndAnnotation");
+    }
+
+}
